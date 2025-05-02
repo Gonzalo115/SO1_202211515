7d2331464dfea3d84fe81a539266f9efda88e443
Gonzalo115 <3513074510101@ingenieria.usac.edu.gt>
2025-05-02 12:50:55 -0600

    configuracion de redis

--- Proyecto2/kafka-consumer/app.go
+++ Proyecto2/kafka-consumer/app.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/segmentio/kafka-go"
 )
 
@@ -41,6 +44,20 @@ func main() {
 
 	log.Printf("Consumidor Kafka iniciado para topic %s, broker %s", topic, broker)
 
+	// Inicar sesion en Redis
+
+	ctx := context.Background()
+
+    redisHost := os.Getenv("REDIS_HOST")
+    redisPassword := os.Getenv("REDIS_PASSWORD")
+
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     redisHost,
+        Password: redisPassword,
+        DB:       0,
+    })
+
+
 	for {
 		m, err := r.ReadMessage(context.Background())
 		if err != nil {
@@ -55,6 +72,20 @@ func main() {
 			continue
 		}
 
-		log.Printf("Mensaje recibido: offset=%d, partition=%d, value=%+v", m.Offset, m.Partition, data)
+        timestamp := time.Now().Format("20060102_150405")
+        key := fmt.Sprintf("registro:%s", timestamp)
+
+		// Grabar mensaje en redis
+
+        err = rdb.Set(ctx, key, m.Value, 0).Err()
+        if err != nil {
+            log.Printf("Error al guardar en Redis: %v\n", err)
+        } else {
+            log.Printf("Registro guardado [%s]: %s\n", key, m.Value)
+        }
+
+
+
+		log.Printf("Mensaje recibido: offset=%d, partition=%d, value=%+v, y guardado en Redis", m.Offset, m.Partition, data)
 	}
 }
